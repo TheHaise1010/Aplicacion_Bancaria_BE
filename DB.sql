55a3e418211c922cdc3013432896e14350f0d0d2
GaranNoGabimaru <jordan.zelayahs@gmail.com>
2025-04-16 16:12:49 -0600

    added new table credenciales to db

--- DB.sql
+++ DB.sql
@@ -21,3 +21,14 @@ INSERT INTO cliente (dui) VALUES ('12345678-9');
 -- Supongamos que el ID asignado al cliente es 1, inserta dos cuentas para ese cliente:
 INSERT INTO cuenta (numero, saldo, cliente_id) VALUES ('0001', 1000, 1);
 INSERT INTO cuenta (numero, saldo, cliente_id) VALUES ('0002', 500, 1);
+
+CREATE TABLE credenciales (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    correo VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    tipo_cuenta ENUM('cliente','empleado') NOT NULL,
+    cliente_dui VARCHAR(10),
+    -- si es cliente, cliente_dui NO puede ser NULL; si es empleado, cliente_dui deberá quedar en NULL
+    CHECK (tipo_cuenta <> 'cliente' OR cliente_dui IS NOT NULL),
+    FOREIGN KEY (cliente_dui) REFERENCES cliente(dui)
+);
