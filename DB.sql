2cc509089d09721763033a280d5ce5f04cd627b2
GaranNoGabimaru <jordan.zelayahs@gmail.com>
2025-04-23 20:25:45 -0600

    corrected credenciales table logic so that only one credencial can be created per DUI

--- DB.sql
+++ DB.sql
@@ -11,6 +11,8 @@ CREATE TABLE cliente (
     fecha_nacimiento DATE NOT NULL
 );
 
+select * from cliente
+
 CREATE TABLE cuenta (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     numero VARCHAR(20) NOT NULL,
@@ -18,6 +20,8 @@ CREATE TABLE cuenta (
     cliente_id BIGINT,
     FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
+
+select * from cuenta
 
 -- Inserta un cliente con DUI '12345678-9'
 INSERT INTO cliente (dui, primer_nombre, apellido, fecha_nacimiento)
@@ -29,11 +33,12 @@ INSERT INTO cuenta (numero, saldo, cliente_id) VALUES ('0002', 500, 1);
 
 CREATE TABLE credenciales (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    correo VARCHAR(50) NOT NULL,
+    correo VARCHAR(50) NOT NULL UNIQUE, -- Es buena práctica hacer el correo único también
     contrasena VARCHAR(10) NOT NULL,
     tipo_cuenta ENUM('cliente','empleado') NOT NULL,
     cliente_dui VARCHAR(10),
     -- si es cliente, cliente_dui NO puede ser NULL; si es empleado, cliente_dui deberá quedar en NULL
     CHECK (tipo_cuenta <> 'cliente' OR cliente_dui IS NOT NULL),
-    FOREIGN KEY (cliente_dui) REFERENCES cliente(dui)
+    FOREIGN KEY (cliente_dui) REFERENCES cliente(dui),
+    UNIQUE (cliente_dui) -- Agregamos esta restricción de unicidad
 );
